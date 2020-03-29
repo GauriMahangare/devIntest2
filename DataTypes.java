b731f46fc42f1fbbeff27317fe74ecd5fad16d8f
Gauri <gauridhumal@gmail.com>
2020-03-29 17:16:09 +0100

    commiting my first project
    Signed-off-by: Gauri <gauridhumal@gmail.com>

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+package com.datatypes;
+import java.io.File;
+public class DataTypes {
+    //primitive DataTypes
+    //Numeric - Integers
+    byte myByte = 127;
+    short myShort = 32765;
+//long needs l or L
+//Float F 7 Decimal places
+
+    // Reference data types --~> These are classes. Wrapper classes Variable that is a class.
+    public String str = new String("abc");
+    File F2 = new File ("ReadMe.txt");
+
+}
